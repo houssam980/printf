2078a32c3c458015baade1e0254e3894cdb34c6b
brahimrz <rzbrahim5@gmail.com>
2023-09-19 14:40:08 -0700

    prjtpn

--- print_number.c
+++ print_number.c
@@ -12,7 +12,7 @@ return (c >= '0' && c <= '9');
 /**
  * _strlen - returns the length of a string
  * @s: string whose length to check
- * Return: the integer length of string
+ * Return: integer length of string
  */
 int _strlen(char *s)
 {
